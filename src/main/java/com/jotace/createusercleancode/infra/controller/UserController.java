7fec903e1bbfa27cae46126d6e1d61fa1a06967f
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-12-09 14:35:37 -0300

    Create Controller

---
+++ src/main/java/com/jotace/createusercleancode/infra/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.jotace.createusercleancode.infra.controller;
+
+import com.jotace.createusercleancode.application.boundary.UserInputBoundary;
+import com.jotace.createusercleancode.application.model.UserRequestModel;
+import com.jotace.createusercleancode.application.model.UserResponseModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    private final UserInputBoundary userInputBoundary;
+
+    public UserController(UserInputBoundary userInputBoundary) {
+        this.userInputBoundary = userInputBoundary;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseModel> createUser(@RequestBody UserRequestModel userRequestModel) {
+       return ResponseEntity.ok(userInputBoundary.create(userRequestModel));
+    }
+    @GetMapping
+    public ResponseEntity<List<UserResponseModel>> getAllUsers() {
+        return ResponseEntity.ok(userInputBoundary.getAllUsers());
+    }
+
+
+}
