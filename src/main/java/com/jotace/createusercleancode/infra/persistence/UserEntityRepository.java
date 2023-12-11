8c95eab3d1760a4f64aade87cf3b93f5c0b14fe4
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-12-10 22:40:19 -0300

    Implementing news methods

--- src/main/java/com/jotace/createusercleancode/infra/persistence/UserEntityRepository.java
+++ src/main/java/com/jotace/createusercleancode/infra/persistence/UserEntityRepository.java
@@ -2,6 +2,7 @@ package com.jotace.createusercleancode.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +13,6 @@ public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 
     @Query("SELECT u FROM users u")
     List<UserEntity> findAllUserEntity();
+    @Query("SELECT u FROM users u WHERE u.id = :userId")
+    UserEntity findUserById(@Param("userId") Long id);
 }
