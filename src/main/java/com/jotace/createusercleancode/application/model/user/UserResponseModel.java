0dc1f3657ec43f21cec542ca5c74e421f9b38b01
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-12-13 11:48:13 -0300

    configuring new models

--- src/main/java/com/jotace/createusercleancode/application/model/user/UserResponseModel.java
+++ src/main/java/com/jotace/createusercleancode/application/model/user/UserResponseModel.java
@@ -3,15 +3,18 @@ package com.jotace.createusercleancode.application.model.user;
 import com.jotace.createusercleancode.core.entity.user.User;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record UserResponseModel(
         Long id,
         String name,
         LocalDateTime creationTIme,
-        String email
+        String email,
+        List<UserPostResponse> userPostResponse
 ) {
     public UserResponseModel(User user) {
-        this(user.getId(), user.getName(), LocalDateTime.now(), user.getEmail());
+        this(user.getId(), user.getName(), LocalDateTime.now(), user.getEmail(),
+                user.getPosts().stream().map(UserPostResponse::new).toList());
     }
 
 }
