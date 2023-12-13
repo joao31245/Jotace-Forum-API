ca81cf65b8ad26b0661e54ea7a395e7445ed2a4f
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-12-13 11:46:22 -0300

    moving files

--- src/main/java/com/jotace/createusercleancode/infra/controller/user/UserController.java
+++ src/main/java/com/jotace/createusercleancode/infra/controller/user/UserController.java
@@ -1,10 +1,11 @@
-package com.jotace.createusercleancode.infra.controller;
+package com.jotace.createusercleancode.infra.controller.user;
 
 import com.jotace.createusercleancode.application.boundary.user.UserInputBoundary;
 import com.jotace.createusercleancode.application.model.user.UserRequestModel;
 import com.jotace.createusercleancode.application.model.user.UserResponseModel;
 import com.jotace.createusercleancode.application.model.user.UserUpdateRequestModel;
 import com.jotace.createusercleancode.application.model.user.UserUpdateResponseModel;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +19,7 @@ public class UserController {
         this.userInputBoundary = userInputBoundary;
     }
     @PostMapping
-    public ResponseEntity<UserResponseModel> createUser(@RequestBody UserRequestModel userRequestModel) {
+    public ResponseEntity<UserResponseModel> createUser(@RequestBody @Valid UserRequestModel userRequestModel) {
        return ResponseEntity.ok(userInputBoundary.create(userRequestModel));
     }
     @GetMapping
@@ -26,7 +27,7 @@ public class UserController {
         return ResponseEntity.ok(userInputBoundary.getAllUsers());
     }
     @PutMapping
-    public ResponseEntity<UserUpdateResponseModel> updateUser(@RequestBody UserUpdateRequestModel requestModel) {
+    public ResponseEntity<UserUpdateResponseModel> updateUser(@RequestBody @Valid UserUpdateRequestModel requestModel) {
        return ResponseEntity.ok(userInputBoundary.update(requestModel));
     }
     @GetMapping("{id}")
