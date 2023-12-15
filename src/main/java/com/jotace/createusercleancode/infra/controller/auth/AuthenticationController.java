56f2e9c42c822d668d3ddb671738fdfc96dc1d0b
joao31245 <134329276+joao31245@users.noreply.github.com>
2023-12-14 22:36:23 -0300

    Create login controller

---
+++ src/main/java/com/jotace/createusercleancode/infra/controller/auth/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.jotace.createusercleancode.infra.controller.auth;
+
+import com.jotace.createusercleancode.application.boundary.user.auth.AuthInputBoundary;
+import com.jotace.createusercleancode.application.model.auth.AuthRequestModel;
+import com.jotace.createusercleancode.application.model.auth.AuthResponseModel;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.naming.AuthenticationException;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    private final AuthInputBoundary authInputBoundary;
+
+    public AuthenticationController(AuthInputBoundary authInputBoundary) {
+        this.authInputBoundary = authInputBoundary;
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<AuthResponseModel> login(@RequestBody @Valid AuthRequestModel requestModel) throws AuthenticationException {
+        return ResponseEntity.ok(authInputBoundary.login(requestModel));
+    }
+}
